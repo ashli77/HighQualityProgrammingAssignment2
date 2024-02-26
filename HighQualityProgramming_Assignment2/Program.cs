d269a25d694798d4f60a010049a50881c4f1b40b
hp <hp@LAPTOP-P49JC8OB>
2024-02-25 19:56:25 -0500

    Created classes and methods

--- HighQualityProgramming_Assignment2/Program.cs
+++ HighQualityProgramming_Assignment2/Program.cs
@@ -1,148 +1,154 @@
-﻿using System;
+﻿using System.Numerics;
 
-class Position
+namespace GemHunter
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public Position(int x, int y)
+    public class Board
     {
-        X = x;
-        Y = y;
-    }
-}
+        #region properties
+        public Cell[,] Grid { get; }
+        private static bool isBoardInitialized { get; set; }
 
-class Player
-{
-    public string Name { get; }
-    public Position Position { get; set; }
-    public int GemCount { get; set; }
+        #endregion
 
-    public Player(string name, Position position)
-    {
-        Name = name;
-        Position = position;
-        GemCount = 0;
-    }
-
-    public void Move(char direction)
-    {
-        switch (direction)
+        #region constructor
+        /// <summary>
+        /// Initializes the board with players, gems, and obstacles.
+        /// </summary>
+        public Board()
         {
-            case 'U':
-                if (Position.Y > 0)
-                    Position.Y--;
-                break;
-            case 'D':
-                if (Position.Y < 5)
-                    Position.Y++;
-                break;
-            case 'L':
-                if (Position.X > 0)
-                    Position.X--;
-                break;
-            case 'R':
-                if (Position.X < 5)
-                    Position.X++;
-                break;
-            default:
-                break;
+            Grid = new Cell[6, 6];
+            InitializeBoard();
         }
-    }
-}
+        #endregion
 
-class Cell
-{
-    public string Occupant { get; set; }
-
-    public Cell()
-    {
-        Occupant = "-";
-    }
-}
-
-class Board
-{
-    public Cell[,] Grid { get; }
-
-    public Board()
-    {
-        Grid = new Cell[6, 6];
-        InitializeBoard();
-    }
-
-    private void InitializeBoard()
-    {
-        // Set up players
-        Grid[0, 0].Occupant = "P1";
-        Grid[5, 5].Occupant = "P2";
-
-        // Set up gems (random positions)
-        Random rand = new Random();
-        for (int i = 0; i < 10; i++)
+        #region methods
+        /// <summary>
+        /// Prints the current state of the board to the console.
+        /// </summary>
+        public void Display()
         {
-            int x = rand.Next(6);
-            int y = rand.Next(6);
-            if (Grid[x, y].Occupant == "-")
-                Grid[x, y].Occupant = "G";
-            else
-                i--;
-        }
-
-        // Set up obstacles (random positions)
-        for (int i = 0; i < 8; i++)
-        {
-            int x = rand.Next(6);
-            int y = rand.Next(6);
-            if (Grid[x, y].Occupant == "-")
-                Grid[x, y].Occupant = "O";
-            else
-                i--;
-        }
-    }
-
-    public void Display()
-    {
-        for (int y = 0; y < 6; y++)
-        {
-            for (int x = 0; x < 6; x++)
+            for (int i = 0; i < Grid.GetLength(0); i++)
             {
-                Console.Write(Grid[x, y].Occupant + " ");
+                for (int j = 0; j < Grid.GetLength(1); j++)
+                {
+                    Console.Write(Grid[i, j].Occupant + " ");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine();
         }
-    }
 
-    public bool IsValidMove(Player player, char direction)
-    {
-        int newX = player.Position.X;
-        int newY = player.Position.Y;
-
-        switch (direction)
+        /// <summary>
+        /// Checks if the move is valid.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public bool IsValidMove(Player player, char direction)
         {
-            case 'U':
-                newY--;
-                break;
-            case 'D':
-                newY++;
-                break;
-            case 'L':
-                newX--;
-                break;
-            case 'R':
-                newX++;
-                break;
-            default:
-                break;
+            int newX = player.Position.X;
+            int newY = player.Position.Y;
+
+            // Update new position based on the direction
+            switch (direction)
+            {
+                case 'U':
+                    newX--;
+                    break;
+                case 'D':
+                    newX++;
+                    break;
+                case 'L':
+                    newY--;
+                    break;
+                case 'R':
+                    newY++;
+                    break;
+                default:
+                    return false; // Invalid direction
+            }
+            // Check if the new position is within the bounds of the board
+            if (newX < 0 || newX >= Grid.GetLength(0) || newY < 0 || newY >= Grid.GetLength(1))
+            {
+                return false; // Move is out of bounds
+            }
+
+            // Check if the new position contains an obstacle
+            if (Grid[newX, newY].Occupant == "O")
+            {
+                return false; // Move is blocked by an obstacle
+            }
+
+            return true; // Move is valid
         }
 
-        if (newX < 0 || newX >= 6 || newY < 0 || newY >= 6)
-            return false;
+        /// <summary>
+        /// Checks if the player's new position contains a gem and updates the player's GemCount.
+        /// </summary>
+        /// <param name="player"></param>
+        public void CollectGem(Player player, char direction)
+        {
+            int x = player.Position.X;
+            int y = player.Position.Y;
 
-        if (Grid[newX, newY].Occupant == "O")
-            return false;
+            switch (direction)
+            {
+                case 'U':
+                    x--;
+                    break;
+                case 'D':
+                    x++;
+                    break;
+                case 'L':
+                    y--;
+                    break;
+                case 'R':
+                    y++;
+                    break;
+            }
 
-        return true;
+            // Check if the player's new position contains a gem
+            if (Grid[x, y].Occupant == "G")
+            {
+                // Update player's GemCount
+                player.GemCount++;
+
+                // Remove the gem from the board
+                Grid[x, y].Occupant = "-";
+            }
+        }
+
+        private void InitializeBoard()
+        {
+            if (isBoardInitialized)
+            {
+                return; // Board has already been initialized
+            }
+
+            for (int i = 0; i < Grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < Grid.GetLength(1); j++)
+                {
+                    Grid[i, j] = new Cell { Occupant = "-" };
+                }
+            }
+
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int gemX = random.Next(Grid.GetLength(0));
+                int gemY = random.Next(Grid.GetLength(1));
+                Grid[gemX, gemY].Occupant = "G";
+
+                int obstacleX = random.Next(Grid.GetLength(0));
+                int obstacleY = random.Next(Grid.GetLength(1));
+                Grid[obstacleX, obstacleY].Occupant = "O";
+            }
+
+            Grid[0, 0].Occupant = "P1";
+            Grid[5, 5].Occupant = "P2";
+            isBoardInitialized = true;
+        }
+        #endregion
     }
-
-    
+}
